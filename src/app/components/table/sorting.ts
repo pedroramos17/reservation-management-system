0ab347aea6c1983e0e55a0eb12e22ea6d344debb
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-01-07 12:01:11 -0300

    refactor: sorting strings with localeCompare

--- src/app/components/table/sorting.ts
+++ src/app/components/table/sorting.ts
@@ -1,23 +1,32 @@
-export type Order = 'asc' | 'desc';
+export type Order = "asc" | "desc";
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
+	if (typeof a[orderBy] === "string") {
+		if ((a[orderBy] as string).localeCompare(b[orderBy] as string)) {
+			return -1;
+		}
+		if ((b[orderBy] as string).localeCompare(a[orderBy] as string)) {
+			return 1;
+		}
+		return 0;
+	}
+	if (b[orderBy] < a[orderBy]) {
+		return -1;
+	}
+	if (b[orderBy] > a[orderBy]) {
+		return 1;
+	}
+	return 0;
 }
 
 export default function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key,
+	order: Order,
+	orderBy: Key
 ): (
-  a: { [key in Key]: number | string | boolean },
-  b: { [key in Key]: number | string | boolean },
+	a: { [key in Key]: number | string | boolean },
+	b: { [key in Key]: number | string | boolean }
 ) => number {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
+	return order === "desc"
+		? (a, b) => descendingComparator(a, b, orderBy)
+		: (a, b) => -descendingComparator(a, b, orderBy);
 }
