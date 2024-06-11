8cedc572ee17ae6a43f52bb442d3a6efea3696dd
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-06-11 01:36:54 -0300

    refactor: fixing alphabetical sorting

--- src/app/components/table/sorting.ts
+++ src/app/components/table/sorting.ts
@@ -1,15 +1,6 @@
 export type Order = "asc" | "desc";
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-	if (typeof a[orderBy] === "string") {
-		if ((a[orderBy] as string).localeCompare(b[orderBy] as string)) {
-			return -1;
-		}
-		if ((b[orderBy] as string).localeCompare(a[orderBy] as string)) {
-			return 1;
-		}
-		return 0;
-	}
 	if (b[orderBy] < a[orderBy]) {
 		return -1;
 	}
