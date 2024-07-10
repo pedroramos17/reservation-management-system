9cae32545e0bc6a2ac1f73effc0dcfeb3609a46c
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-07-10 13:17:06 -0300

    refactor: fix redux provider in next

---
+++ src/app/StoreProvider.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { Provider } from "react-redux";
+import { AppStore, makeStore } from "@/lib/store";
+import { useRef } from "react";
+
+export default function StoreProvider({ children }: Readonly<{ children: React.ReactNode }>) {
+    const storeRef = useRef<AppStore>();
+    if (!storeRef.current) {
+        storeRef.current = makeStore();
+    }
+    return <Provider store={storeRef.current}>{children}</Provider>;
+}
