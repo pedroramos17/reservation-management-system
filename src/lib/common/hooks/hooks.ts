bacc60f3b6b1211a549d828034ab07a507788b3c
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-07-15 03:55:48 -0300

    refactor: clean import

--- src/lib/common/hooks/hooks.ts
+++ src/lib/common/hooks/hooks.ts
@@ -1,6 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector, useStore } from "react-redux";
 import type { RootState, AppDispatch, AppStore } from "@/lib/store";
-import { useStore } from "react-redux";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
