db374ea6ff072f03c3346e549fd68ede86a2708f
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-07-15 03:58:11 -0300

    refactor: normalize data with entity adapter

--- src/lib/features/drivers/driversSlice.ts
+++ src/lib/features/drivers/driversSlice.ts
@@ -1,169 +1,129 @@
 import {
+	createAsyncThunk,
+	createEntityAdapter,
+	createSlice,
+} from "@reduxjs/toolkit";
+import { normalize, schema } from "normalizr";
+import {
 	getStoreData,
 	addData,
 	updateData,
 	Stores,
 	deleteData,
+	Driver,
 } from "@/lib/utils/db";
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface DataProps {
-	id: string;
-	updatedAt: string;
-	createdAt: string;
-}
-
-export interface Vehicle extends DataProps {
-	brand: string;
-	model: string;
-	year: number;
-	color: string;
-	plate: string;
-}
-
-export interface Driver extends DataProps {
-	name: string;
-	rg: string;
-	phone: string;
-	vehicles: Vehicle[];
-}
-interface StateProps {
-	loading: boolean;
+interface DriversState {
+	entities: {} | Driver[];
+	ids: string[];
+	loading: "idle" | "pending" | "succeeded" | "failed";
 	error: string | null;
 }
-interface DriverState extends StateProps {
-	drivers: Record<string, Driver>[];
-}
 
-const drivers = createAsyncThunk("driver/getAll", async () => {
+type AtLeastOne<T extends Record<string, any>> = keyof T extends infer K
+	? K extends string
+		? Pick<T, K & keyof T> & Partial<T>
+		: never
+	: never;
+
+type AtLeastOneDriverField = AtLeastOne<Driver>;
+
+const driverEntity = new schema.Entity<Driver>("drivers");
+
+const getDrivers = createAsyncThunk("driver/getAll", async () => {
 	const drivers = await getStoreData<Driver>(Stores.Drivers);
-	return drivers;
+	const normalized = normalize(drivers, driverEntity);
+	return normalized.entities;
 });
 
 const addDriver = createAsyncThunk("driver/add", async (data: Driver) => {
-	const driverAdded = await addData<Driver>(Stores.Drivers, data);
-	return driverAdded;
+	return (await addData<Driver>(Stores.Drivers, data)) as Driver;
 });
 
 const updateDriver = createAsyncThunk(
 	"driver/update",
-	async (updatedData: Partial<Driver> & { id: string }) => {
-		const driverUpdated = await updateData(
+	async (updatedData: AtLeastOneDriverField & { id: string }) => {
+		return (await updateData(
 			Stores.Drivers,
 			updatedData.id,
 			updatedData
-		);
-		return driverUpdated;
+		)) as AtLeastOneDriverField;
 	}
 );
 
 const deleteDriver = createAsyncThunk("driver/delete", async (id: string) => {
-	const driverIdDeleted = await deleteData(Stores.Drivers, id);
-	return driverIdDeleted;
+	return (await deleteData(Stores.Drivers, id)) as string;
 });
+
+export const driversAdapter = createEntityAdapter<Driver>();
+
+const initialState = driversAdapter.getInitialState({
+	loading: "idle",
+	error: null as string | null,
+});
+
 export const driverSlice = createSlice({
-	name: "driver",
-	initialState: {
-		drivers: {},
-		loading: false,
-		error: null,
-	} as DriverState,
+	name: "drivers",
+	initialState,
 	reducers: {},
 	extraReducers: (builder) => {
 		builder
-			.addCase(drivers.pending, (state) => {
-				state.loading = true;
+			.addCase(getDrivers.pending, (state) => {
+				state.loading = "pending";
 				state.error = null;
 			})
-			.addCase(drivers.fulfilled, (state, action) => {
-				state.loading = false;
-				state.drivers = action.payload.reduce(
-					(drivers, driver) => {
-						const { id } = driver;
-						drivers = {
-							...drivers,
-							[id]: driver,
-						};
-						return drivers;
-					},
-					{ ...state.drivers }
-				);
+			.addCase(getDrivers.fulfilled, (state, { payload }) => {
+				state.loading = "succeeded";
+				driversAdapter.upsertMany(state, payload.drivers!);
 			})
-			.addCase(drivers.rejected, (state, action) => {
-				state.loading = false;
+			.addCase(getDrivers.rejected, (state, action) => {
+				state.loading = "failed";
 				state.error = action.error.message ?? "Failed to get drivers";
 			})
 			.addCase(addDriver.pending, (state) => {
-				state.loading = true;
+				state.loading = "pending";
 				state.error = null;
 			})
-			.addCase(addDriver.fulfilled, (state, action) => {
-				state.loading = false;
-				if (isDriver(action.payload)) {
-					const driver = action.payload;
-					const { id } = driver;
-					state.drivers = {
-						...state.drivers,
-						[id]: driver,
-					};
-				} else {
-					state.error = "Invalid driver payload";
-				}
+			.addCase(addDriver.fulfilled, (state, { payload }) => {
+				state.loading = "succeeded";
+				driversAdapter.addOne(state, payload);
 			})
 			.addCase(addDriver.rejected, (state, action) => {
-				state.loading = false;
+				state.loading = "failed";
 				state.error = action.error.message ?? "Failed to add driver";
 			})
 			.addCase(updateDriver.pending, (state) => {
-				state.loading = true;
+				state.loading = "pending";
 				state.error = null;
 			})
-			.addCase(updateDriver.fulfilled, (state, action) => {
-				state.loading = false;
-				if (isDriver(action.payload)) {
-					const driver = action.payload;
-					const { id } = driver;
-					if (isDriver(action.payload)) {
-						state.drivers = {
-							...state.drivers,
-							[id]: {
-								...driver,
-							},
-						};
-					}
+			.addCase(updateDriver.fulfilled, (state, { payload }) => {
+				state.loading = "succeeded";
+				const { id, ...changes } = payload;
+				if (id) {
+					driversAdapter.updateOne(state, { id, changes });
 				} else {
-					state.error = "Invalid driver payload";
+					state.error = "Failed to update driver, id not found";
 				}
 			})
 			.addCase(updateDriver.rejected, (state, action) => {
-				state.loading = false;
+				state.loading = "failed";
 				state.error = action.error.message ?? "Failed to update driver";
 			})
 			.addCase(deleteDriver.pending, (state) => {
-				state.loading = true;
+				state.loading = "pending";
 				state.error = null;
 			})
-			.addCase(deleteDriver.fulfilled, (state, action) => {
-				state.loading = false;
-				if (isDriver(action.payload)) {
-					const driver = action.payload;
-					const { id } = driver;
-					state.drivers = state.drivers.filter(() => !id);
-				} else {
-					state.error = "Invalid driver payload";
-				}
+			.addCase(deleteDriver.fulfilled, (state, { payload }) => {
+				state.loading = "succeeded";
+				driversAdapter.removeOne(state, payload);
 			})
 			.addCase(deleteDriver.rejected, (state, action) => {
-				state.loading = false;
+				state.loading = "failed";
 				state.error = action.error.message ?? "Failed to delete driver";
 			});
 	},
 });
 
-function isDriver(
-	payload: string | Driver | (Partial<Driver> & { id: string }) | null
-): payload is Driver {
-	return payload !== null && typeof payload === "object";
-}
-
-export default driverSlice.reducer;
+const { reducer } = driverSlice;
+export { getDrivers, addDriver, updateDriver, deleteDriver };
+export default reducer;
