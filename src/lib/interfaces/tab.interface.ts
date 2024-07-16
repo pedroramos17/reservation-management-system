c5dd6effa841f6126eb17cbd6b29e44a770db78a
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-07-16 07:35:07 -0300

    refactor: remove tabs

--- src/lib/interfaces/tab.interface.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export interface HandleTabsProps {
-	handleParkedTab: (
-		event: React.SyntheticEvent<Element, Event> | null,
-		value: number | string | null
-	) => void;
-}
