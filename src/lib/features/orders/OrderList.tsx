73020876daad3bdd23f1b12174a5d42d771aecd5
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-09-17 01:12:59 -0300

    style: remove breakline

--- src/lib/features/orders/OrderList.tsx
+++ src/lib/features/orders/OrderList.tsx
@@ -23,7 +23,6 @@ export default function OrderList() {
     return (
         <div>
             <h1>Lista de Pedidos</h1>
-
             {orders && (
                 <div>
                     <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: 10 }}>
