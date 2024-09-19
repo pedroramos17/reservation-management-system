fd64fd073e44da842f84a0d5d86433f05e105f26
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-09-18 23:38:23 -0300

    feat(app): properties page

---
+++ src/app/propriedades/criar/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { Container, Title } from "@/lib/common/components/styles";
+import PropertyForm from "@/lib/features/properties/PropertyForm";
+
+export default function Page() {
+
+    return (<PropertyForm id="" />)
+}
