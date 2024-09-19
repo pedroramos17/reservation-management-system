6808b774ca610fbc60b977b5ae6aa5fbb36e3e90
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-09-18 23:34:04 -0300

    change utils file to dir

---
+++ src/lib/features/utils/currencyCodeList.ts
@@ -0,0 +1,177 @@
+export const currencyCodeList = [
+	{
+		countryCode: "af",
+		defaultCurrency: "AFN",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "ar",
+		defaultCurrency: "USD",
+		additionalCurrency: "ARS",
+	},
+	{
+		countryCode: "bg",
+		defaultCurrency: "BGN",
+		additionalCurrency: "EUR",
+	},
+	{
+		countryCode: "bj",
+		defaultCurrency: "EUR",
+		additionalCurrency: "XOF",
+	},
+	{
+		countryCode: "br",
+		defaultCurrency: "BRL",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "cl",
+		defaultCurrency: "USD",
+		additionalCurrency: "CLP",
+	},
+	{
+		countryCode: "co",
+		defaultCurrency: "COP",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "cz",
+		defaultCurrency: "EUR",
+		additionalCurrency: "CZK",
+	},
+	{
+		countryCode: "ga",
+		defaultCurrency: "EUR",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "ge",
+		defaultCurrency: "GEL",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "gn",
+		defaultCurrency: "EUR",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "gt",
+		defaultCurrency: "GTQ",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "hu",
+		defaultCurrency: "EUR",
+		additionalCurrency: "HUF",
+	},
+	{
+		countryCode: "id",
+		defaultCurrency: "IDR",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "il",
+		defaultCurrency: "ILS",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "is",
+		defaultCurrency: "EUR",
+		additionalCurrency: "ISK",
+	},
+	{
+		countryCode: "ma",
+		defaultCurrency: "EUR",
+		additionalCurrency: "MAD",
+	},
+	{
+		countryCode: "md",
+		defaultCurrency: "MDL",
+		additionalCurrency: "EUR",
+	},
+	{
+		countryCode: "ml",
+		defaultCurrency: "XOF",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "mo",
+		defaultCurrency: "HKD",
+		additionalCurrency: "MOP",
+	},
+	{
+		countryCode: "mu",
+		defaultCurrency: "EUR",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "mx",
+		defaultCurrency: "USD",
+		additionalCurrency: "MXN",
+	},
+	{
+		countryCode: "my",
+		defaultCurrency: "MYR",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "mz",
+		defaultCurrency: "MZN",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "ng",
+		defaultCurrency: "USD",
+		additionalCurrency: "NGN",
+	},
+	{
+		countryCode: "pk",
+		defaultCurrency: "PKR",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "ro",
+		defaultCurrency: "RON",
+		additionalCurrency: "EUR",
+	},
+	{
+		countryCode: "sl",
+		defaultCurrency: "USD",
+		additionalCurrency: "SLL",
+	},
+	{
+		countryCode: "th",
+		defaultCurrency: "THB",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "tn",
+		defaultCurrency: "EUR",
+		additionalCurrency: "TND",
+	},
+	{
+		countryCode: "tr",
+		defaultCurrency: "EUR",
+		additionalCurrency: "TRY",
+	},
+	{
+		countryCode: "ua",
+		defaultCurrency: "UAH",
+		additionalCurrency: "EUR",
+	},
+	{
+		countryCode: "uz",
+		defaultCurrency: "USD",
+		additionalCurrency: "UZS",
+	},
+	{
+		countryCode: "vn",
+		defaultCurrency: "VND",
+		additionalCurrency: "USD",
+	},
+	{
+		countryCode: "ws",
+		defaultCurrency: "USD",
+		additionalCurrency: "WST",
+	},
+];
