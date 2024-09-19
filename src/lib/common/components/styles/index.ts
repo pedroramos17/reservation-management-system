044f7a52985774b2182a64f2bc2825caceb0d1b3
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-09-18 23:22:40 -0300

    add title component and container is 100vh

--- src/lib/common/components/styles/index.ts
+++ src/lib/common/components/styles/index.ts
@@ -1,12 +1,16 @@
 import styled from "@emotion/styled";
 
+export const Title = styled("h1")({
+	color: "#444746",
+});
+
 export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
 	align-items: start;
 	overflow: auto;
 	padding: 0 20px;
+	height: 100vh;
 `;
 export const HeaderContainer = styled.div`
 	display: flex;
