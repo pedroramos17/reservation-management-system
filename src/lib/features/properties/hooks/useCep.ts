9a37bde147767b235333421c53959fff194a7112
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-10-01 12:19:07 -0300

    use opencep api

--- src/lib/features/properties/hooks/useCep.ts
+++ src/lib/features/properties/hooks/useCep.ts
@@ -2,18 +2,7 @@
 
 import useSWR from "swr";
 
-const headers = new Headers();
-headers.append("Access-Control-Allow-Origin", "http://localhost:3000");
-headers.append("Access-Control-Allow-Credentials", "true");
-headers.append("Access-Control-Allow-Origin", "*");
-headers.append("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-headers.append(
-	"Access-Control-Allow-Headers",
-	"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-);
-
-const fetcher = (url: string) =>
-	fetch(url, { headers }).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function useCep({
 	postalCode,
@@ -23,7 +12,7 @@ export function useCep({
 	shouldFetch?: boolean;
 }) {
 	const { data, error, isLoading } = useSWR(
-		shouldFetch ? `https://viacep.com.br/ws/${postalCode}/json/` : null,
+		shouldFetch ? `https://opencep.com/v1/${postalCode}` : null,
 		fetcher
 	);
 	return {
