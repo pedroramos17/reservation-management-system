cfb6459f7b1f460cd07f81f72e13ccb6af5a2459
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-10-01 21:16:27 -0300

    error handling

--- src/lib/features/properties/hooks/useCep.ts
+++ src/lib/features/properties/hooks/useCep.ts
@@ -2,7 +2,19 @@
 
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = async (url: string) => {
+	try {
+		const res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error("Failed to fetch cep");
+		}
+
+		return await res.json();
+	} catch (e) {
+		throw new Error("Failed to fetch cep");
+	}
+};
 
 export function useCep({
 	postalCode,
@@ -13,7 +25,8 @@ export function useCep({
 }) {
 	const { data, error, isLoading } = useSWR(
 		shouldFetch ? `https://opencep.com/v1/${postalCode}` : null,
-		fetcher
+		fetcher,
+		{ shouldRetryOnError: false }
 	);
 	return {
 		data,
