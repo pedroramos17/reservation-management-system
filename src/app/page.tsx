275363e49f2271f8ce09fc602c8ce80341aa2b47
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-10-11 10:38:36 -0300

    use client only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import 'client-only';
 import { useRouter } from 'next/navigation';
 export default function App() {
   const router = useRouter();
-  const [isDBReady, setIsDBReady] = useState(false);
-
-  // wrap everything in a provider that ensures IndexedDB is ready
-  useEffect(() => {
-    if (indexedDB) {
-      import('@/lib/db/idb').then(() => {
-        setIsDBReady(true);
-      });
-    }
-  }, []);
-
-  if (!isDBReady) {
-    return <div>Loading...</div>;
-  }
-
 
   return router.push('/propriedades');
 }
