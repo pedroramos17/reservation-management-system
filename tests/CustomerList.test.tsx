1ba1d2e0ccbb87c9c9a381eac31ff85a58bc9718
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-10-12 01:00:06 -0300

    test: customer list tests

--- tests/CustomerList.test.tsx
+++ tests/CustomerList.test.tsx
@@ -1,35 +1,64 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import CustomerList, { searchCustomers } from '@/lib/features/customers/CustomerList';
+import { Customer } from '@/lib/db/idb';
 
-import {render, fireEvent, screen} from '@testing-library/react'
-import { useRouter } from 'next/navigation';
-import App from "@/app/page";
-import { http, HttpResponse } from 'msw'
+describe('CustomerList Component', () => {
+  it('renders without crashing', () => {
+      render(<CustomerList query=''/>);
+      expect(screen.getByText(/Motoristas/i)).toBeInTheDocument();
+  });
 
-const server = setupServer(
-  http.get('/', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json([]))
-  })
-)
+});
 
-jest.mock('next/navigation', () => ({
-    useRouter: () => ({
-        replace: jest.fn(),
-        push: jest.fn(),
-        events: {
-          on: jest.fn(),
-          off: jest.fn(),
-          emit: jest.fn(),
-        },
-        isFallback: false,
-    }),
-}));
+describe('searchCustomers function', () => {
+    const customers: Customer[] = [
+      {
+        id: '1',
+        name: 'John Doe',
+        email: 'jdoe@me.com',
+        phone: null,
+        taxpayerRegistration: null,
+        updatedAt: null,
+      },
+      {
+        id: '2',
+        name: 'Mary Jane',
+        email: 'mjane@me.com',
+        phone: null,
+        taxpayerRegistration: null,
+        updatedAt: null,
+      },
+  ];
+  const query = 'Jan';
+  const customersResponse = searchCustomers(query, customers);
+  let searchedCustomersIds = [];
+  customersResponse.forEach((response) => {
+      searchedCustomersIds = response['result'];
+  });
 
-describe('App Component', () => {
-  it('should push to /propriedades on render', () => {
-    const replaceMock = jest.fn();
-    (useRouter as jest.Mock).mockReturnValue({ replace: replaceMock });
+  it('returns empty array with empty query', () => {
+    const query = '';
+    const results = searchCustomers(query, customers);
+    expect(results).toEqual([]);
+  });
 
-    
+  it('returns customer with exact name match', () => {
+    const query = 'John Doe';
+    const results = searchCustomers(query, customers);
+    expect(results[1]).toEqual(customers[0].id);
+  });
 
-    expect(replaceMock).toHaveBeenCalledWith('/propriedades');
+  it('returns customers with partial name match', () => {
+    const query = 'Doe';
+    const results = searchCustomers(query, customers);
+    expect(results[1]).toEqual(customers[0].id);
+
+  });
+
+  it('returns empty result with non-existent customer name', () => {
+    const query = 'Unknown Customer';
+    const results = searchCustomers(query, customers);
+    expect(results).toEqual([]);
   });
 });
