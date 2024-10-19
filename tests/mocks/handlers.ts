475de9ee36d8ff4fa7d26109884e8a91b824351b
pedroramos17 <pedro_ramos2003@hotmail.com>
2024-10-19 13:21:05 -0300

    test: wip handlers

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,11 +1,35 @@
-import { rest } from "msw";
+import { http, HttpResponse } from "msw";
+
+type AddCustomerParams = {
+	query: string;
+};
+
+type AddCustomerRequestBody = {
+	id: string;
+	name: string;
+	car: string;
+	plate: string;
+};
+
+type AddCustomerResponseBody = {
+	id: string;
+};
 
 export const router = {
 	replace: jest.fn(),
 };
-
 export const handlers = [
-	rest.get("/", (req, res, ctx) => {
-		return res(router.replace("/propriedades"));
+	http.post<
+		AddCustomerParams,
+		AddCustomerRequestBody,
+		AddCustomerResponseBody,
+		"/motoristas"
+	>("/motoristas", async ({ params, request }) => {
+		const { query } = params;
+		const { id, name, car, plate } = await request.json();
+
+		return HttpResponse.json({
+			id,
+		});
 	}),
 ];
